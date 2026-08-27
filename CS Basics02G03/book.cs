505d7288b4ff5f24a4b97f0e17b36f95ebc51495
passant ahmed <passantahmed992006@outlook.com>
2026-08-27 06:07:00 +0300

    question01

---
+++ CS Basics02G03/book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Basics02G03
+{
+    internal class Book
+    {
+        public string title { get; set; }
+        public int page { get; set; }
+        public override string ToString()
+        {
+            return $"title: {title},page:{page}";
+        }
+        public override bool Equals(object? obj)
+        {
+            if(obj is not Book)
+                return false;
+            Book book = obj as Book;
+            return book.title == title  && book.page == page ;
+
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(title , page);
+        }
+
+    }
+}
